ff0e282361f2e744447e2399315e2d23a7303e23
DevBrianHQ <kipchumbabrian47@gmail.com>
2024-07-11 12:15:54 +0300

    calculated NHIF

--- Assignments/NetSalaryCalculator.js
+++ Assignments/NetSalaryCalculator.js
@@ -42,3 +42,42 @@ function calculatePAYE(grossSalary) {
     return paye;
 }
 
+function calculateNHIF(grossSalary) {
+    let nhif = 0;
+
+    if (grossSalary <= 5999) {
+        nhif = 150;
+    } else if (grossSalary <= 7999) {
+        nhif = 300;
+    } else if (grossSalary <= 11999) {
+        nhif = 400;
+    } else if (grossSalary <= 14999) {
+        nhif = 500;
+    } else if (grossSalary <= 19999) {
+        nhif = 600;
+    } else if (grossSalary <= 24999) {
+        nhif = 750;
+    } else if (grossSalary <= 29999) {
+        nhif = 850;
+    } else if (grossSalary <= 34999) {
+        nhif = 900;
+    } else if (grossSalary <= 39999) {
+        nhif = 950;
+    } else if (grossSalary <= 44999) {
+        nhif = 1000;
+    } else if (grossSalary <= 49999) {
+        nhif = 1100;
+    } else if (grossSalary <= 59999) {
+        nhif = 1200;
+    } else if (grossSalary <= 69999) {
+        nhif = 1300;
+    } else if (grossSalary <= 79999) {
+        nhif = 1400;
+    } else if (grossSalary <= 89999) {
+        nhif = 1500;
+    } else if (grossSalary >= 100000) {
+        nhif = 1700;
+    }
+
+    return nhif;
+}
